892c088ac9d27e294e542fb72c3eee294f024da5
egesenli <egesenli@gmail.com>
2023-02-27 18:05:47 -0500

    Refactor parent and child components for clean code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,13 +3,13 @@ import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  
-  const itemClass = classNames("interviewers__item", {
+
+  const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected
   });
 
   return (
-    <li className={itemClass} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
