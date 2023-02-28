74d8ce53c2003cc782f044aca945792f99cc1c9a
egesenli <egesenli@gmail.com>
2023-02-28 12:34:14 -0500

    Create story of Appointment component with belonging parts

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,11 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+
+
 
 
 
@@ -136,3 +141,12 @@ storiesOf("InterviewerListItem", module)
       onChange={action("setCurrentInterviewerID")}
     />
   ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
