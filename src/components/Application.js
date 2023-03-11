ae1ecf94e2fb8ca80f1e4722911b9745eebea42b
egesenli <egesenli@gmail.com>
2023-03-10 22:32:26 -0500

    Import getInterviewersForDay selector into Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors";
+
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
@@ -37,14 +38,18 @@ export default function Application(props) {
   }, []);
 
   const appointmentsForDay = getAppointmentsForDay(state, state.day);
+  const interviewersForDay = getInterviewersForDay(state, state.day);
+
   const schedule = appointmentsForDay.map(appointment => {
     const interview = getInterview(state, appointment.interview);
+    const interviewers = interviewersForDay;
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={interviewers}
       />
     );
   });
