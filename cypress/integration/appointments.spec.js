0212add058627f876639b19e637d611c92db2119
egesenli <egesenli@gmail.com>
2023-03-22 19:10:24 -0400

    Add comments to the tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,12 +1,14 @@
+//Cypress tests for the appointment booking app
 describe("Appointments", () => {
   beforeEach(() => {
+    //Reset and seed the database prior to every test
     cy.request("GET", "/api/debug/reset");
-
+    //Visit the root of our web server
     cy.visit("/");
-
+    //Wait for the page to load
     cy.contains("Monday");
   });
-
+  //Test to book an interview
   it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
@@ -20,7 +22,7 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
-
+  //Test to edit an interview
   it("should edit an interview", () => {
     cy.get("[alt=Edit]")
       .first()
@@ -37,16 +39,16 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
-
+  //Test to cancel an interview
   it("should cancel an interview", () => {
     cy.get("[alt=Delete]")
       .click({ force: true });
-  
+
     cy.contains("Confirm").click();
-  
+
     cy.contains("Deleting").should("exist");
     cy.contains("Deleting").should("not.exist");
-  
+
     cy.contains(".appointment__card--show", "Archie Cohen")
       .should("not.exist");
   });
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,8 +1,9 @@
+//Cypress test for navigation
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
-
+  //Test to navigate to Tuesday
   it("should navigate to Tuesday", () => {
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
